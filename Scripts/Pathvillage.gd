d1fcc35330c7fb8b0eb614edd82e0eee3a29abd7
Syl <sylvan@lilo.org>
2018-04-14 22:27:20 +0200

    changes

--- Scripts/Pathvillage.gd
+++ Scripts/Pathvillage.gd
@@ -6,4 +6,5 @@ func ready():
 func _input(event):
 	if event is InputEventMouseButton:
         if event.get_button_index() == BUTTON_RIGHT and event.is_pressed():
-            	get_tree().change_scene("res://Scenes/TESTMAP.tscn")
+            	get_node("/root/Global").player_map_position = Vector2(-497,-164)
+								get_tree().change_scene("res://Scenes/TESTMAP.tscn")
